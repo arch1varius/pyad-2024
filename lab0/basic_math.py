27dbdc00cd2fedf86a1cf8c9fea7866fda0b7230
arch1varius <112974779+arch1varius@users.noreply.github.com>
2025-01-04 08:07:29 +0300

    fixed lab_0

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy as sc
+from scipy.optimize import minimize_scalar
 
 
 def matrix_multiplication(matrix_a, matrix_b):
@@ -13,12 +14,11 @@ def matrix_multiplication(matrix_a, matrix_b):
             for k in range(len(matrix_b)):
                 result[i][j] += matrix_a[i][k] * matrix_b[k][j]
     return result
-    pass
 
 
 def functions(a_1, a_2):
-    extr1 = find_extremum(a_1[0], a_1[1], a_1[2])
-    extr2 = find_extremum(a_2[0], a_2[1], a_2[2])
+    a_1 = list(map(float, a_1.split()))
+    a_2 = list(map(float, a_2.split()))
     a = a_1[0] - a_2[0]
     b = a_1[1] - a_2[1]
     c = a_1[2] - a_2[2]
@@ -29,36 +29,30 @@ def functions(a_1, a_2):
     elif a == 0 and b== 0:
         return []
     elif a == 0:
-        return [(-c / b)]
+        x = (-c / b)
+        return [(x, a_1[0] * x ** 2 + a_1[1] * x + a_1[2])]
     else:
         if discr > 0:
             x1 = (-b + np.sqrt(discr)) / (2 * a)
             x2 = (-b - np.sqrt(discr)) / (2 * a)
-            return [x1, x2]
+            return [(x1, a_1[0] * x1 ** 2 + a_1[1] * x1 + a_1[2]), (x2, a_1[0] * x2 ** 2 + a_1[1] * x2 + a_1[2])]
         elif discr == 0:
-            return [(-b / (2 * a))]
+            x = -b / (2 * a)
+            return [(x, a_1[0] * x ** 2 + a_1[1] * x + a_1[2])]
         else:
             return []
-    pass
 
 
 def skew(x):
     mean = sum(x) / len(x)
     m2 = sum((x1 - mean) ** 2 for x1 in x) / len(x)  # Момент второго порядка (дисперсия)
     m3 = sum((x1 - mean) ** 3 for x1 in x) / len(x)  # Момент третьего порядка
-    return m3 / (m2 ** (3 / 2))
-    pass
+
+    return np.round(m3 / (m2 ** (3 / 2)), decimals=2)
 
 
 def kurtosis(x):
     mean = sum(x) / len(x)
     m2 = sum((x1 - mean) ** 2 for x1 in x) / len(x)  # Момент второго порядка (дисперсия)
     m4 = sum((x1 - mean) ** 4 for x1 in x) / len(x)  # Момент третьего порядка
-    return m4 / (m2 ** 2) - 3
-    pass
-
-def find_extremum(a1, a2, a3):
-    func = lambda x: a1 * x ** 2 + a2 * x + a3
-    res = sc.minimize_scalar(func)
-    return res.x, func(res.x)
-
+    return np.round(m4 / (m2 ** 2) - 3, decimals=2)
